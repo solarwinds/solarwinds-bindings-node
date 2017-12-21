047dc497e77a5a94c5bdb45dd66e47b4b168200f
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2017-12-21 10:33:11 -0800

    Add tests for new methods and signatures. Increase rigor of some tests.

--- test/event.test.js
+++ test/event.test.js
@@ -2,25 +2,58 @@ var bindings = require('../')
 
 describe('addon.event', function () {
   var event
+  var random = bindings.Metadata.makeRandom()
 
   bindings.oboeInit(process.env.APPOPTICS_SERVICE_KEY)
 
-  it('should construct', function () {
+  it('should construct an event', function () {
     event = new bindings.Event()
+  })
+
+  it('should construct an event using metadata', function () {
+    var beginning = random.toString().slice(0, 42)
+    event = new bindings.Event(random)
+    // They should share the same task ID
+    event.toString().slice(0, 42).should.equal(beginning)
   })
 
   it('should add info', function () {
     event.addInfo('key', 'val')
   })
 
-  it('should add edge', function () {
-    var e = new bindings.Event()
-    var meta = e.getMetadata()
+  it('should add edge as metadata', function () {
+    // base the metadata on the same task ID
+    var meta = new bindings.Metadata(random);
     event.addEdge(meta)
   })
 
+  it('should not add edge when task IDs do not match', function () {
+    var md = bindings.Metadata.makeRandom()
+    // force event to a different task ID because oboe returns "OK"
+    // when the event metadata is invalid. And just calling
+    // new Event() results in 2B0000000..0000<op id><flags>, i.e.,
+    // invalid metadata. So it doesn't fail even though it didn't
+    // add an edge. Hmmm.
+    var e = new bindings.Event(bindings.Metadata.makeRandom())
+    console.log(md.toString(1))
+    console.log(e.toString(1))
+    // it seems that .should.throw() doesn't work quite right.
+    var threw = false
+    try {
+      e.addEdge(md)
+    } catch (e) {
+      threw = true
+    }
+    threw.should.equal(true)
+  })
+
+  it('should add edge as an event', function () {
+    var e = new bindings.Event(event)
+    event.addEdge(e)
+  })
+
   it('should add edge as string', function () {
-    var e = new bindings.Event()
+    var e = new bindings.Event(event)
     var meta = e.toString()
     event.addEdge(meta.toString())
   })
@@ -33,13 +66,12 @@ describe('addon.event', function () {
   it('should serialize metadata to id string', function () {
     var meta = event.toString()
     meta.should.be.an.instanceof(String).with.lengthOf(60)
-    meta[0].should.equal('2')
-    meta[1].should.equal('B')
+    meta.should.match(/2B[A-F0-9]{58}/)
   })
 
   it('should start tracing, returning a new instance', function () {
-    var meta = new bindings.Metadata()
-    var event = new bindings.Event(meta, false)
+    var meta = new bindings.Metadata.makeRandom()
+    var event2 = new bindings.Event(meta, false)
     //var event2 = bindings.Event.startTrace(meta)
   })
 })
--- test/metadata.test.js
+++ test/metadata.test.js
@@ -9,9 +9,35 @@ describe('addon.metadata', function () {
     metadata = new bindings.Metadata()
   })
 
-  it('should construct from random data', function () {
+  it('should construct with random data', function () {
     metadata = bindings.Metadata.makeRandom()
     metadata.toString().should.not.equal('')
+    metadata.toString().should.match(/2B[A-F0-9]{56}00/)
+  })
+
+  it('should set the sample flag', function () {
+    var md = metadata.toString()
+    metadata.setSampleFlag()
+    metadata.toString().slice(0, -2).should.equal(md.slice(0, -2))
+    metadata.toString().slice(-2).should.equal('01')
+  })
+
+  it('should clear the sample flag', function () {
+    var md = metadata.toString()
+    metadata.clearSampleFlag()
+    metadata.toString().slice(0, -2).should.equal(md.slice(0, -2))
+    metadata.toString().slice(-2).should.equal('00')
+  })
+
+  it('should construct using metadata', function () {
+    var md = new bindings.Metadata(metadata)
+    md.toString().should.equal(metadata.toString())
+  })
+
+  it('should construct using an event', function () {
+    var e = new bindings.Event(metadata)
+    var md = new bindings.Metadata(e)
+    md.toString().should.equal(e.toString())
   })
 
   it('should serialize to string', function () {
