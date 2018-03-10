c54f416fdf5e5b2b988ee902bd3dcbb2f76e2db5
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2018-03-10 10:35:59 -0800

    Add get-new-oboe argument.

--- env.sh
+++ env.sh
@@ -1,4 +1,5 @@
 ARG=$1
+PARAM=$2
 
 if [[ -z "$AO_TOKEN_STG" ]]; then
     echo "AO_TOKEN_STG must be defined and contain a valid token"
@@ -26,6 +27,36 @@ elif [[ "$ARG" = "ssl" ]]; then
 elif [[ "$ARG" = "debug" ]]; then
     export APPOPTICS_DEBUG_LEVEL=6
     export APPOPTICS_SHOW_GYP=1
+elif [[ "$ARG" = "get-new-oboe" ]]; then
+    # can't really use https://files.appoptics.com/c-lib (only .so and oboe.h)
+    # https://s3-us-west-2.amazonaws.com/rc-files-t2/c-lib/latest/
+    # c-lib/latest/liboboe-1.0-x86_64.so.0.0.0
+    if [[ -z "$PARAM" ]]; then
+        echo "Must supply a version (which will be used as the destination"
+        echo "directory. N.B. this isn't bullet-proof. It presumes sha256sum"
+        echo "exists and that you're running bash."
+        echo "example:"
+        echo "$ . env.sh get-new-oboe latest"
+        return
+    fi
+    OBOE_NAME=liboboe-1.0-x86_64.so.0.0.0
+    URL="https://s3-us-west-2.amazonaws.com/rc-files-t2/c-lib/$PARAM/"
+    mkdir -p "./$PARAM"
+    for f in oboe.h oboe_debug.h VERSION "$OBOE_NAME" "$OBOE_NAME.sha256"
+    do
+        echo "pretending to download $f"
+        #curl -o "./$PARAM/$f" "${URL}$f"
+    done
+    # check the sha256
+    correct=$(cat "./$PARAM/$OBOE_NAME.sha256")
+    read -ra sha256 <<< $(sha256sum "./$PARAM/$OBOE_NAME")
+    if [[ "${sha256[0]}" != "$correct" ]]; then
+        echo "WARNING: SHA256 DOES NOT MATCH!"
+        echo "found    ${sha256[0]} (${#sha256[0]})"
+        echo "expected $correct (${#correct})"
+    fi
+
+
 else
     echo "ERROR $ARG invalid"
 fi
