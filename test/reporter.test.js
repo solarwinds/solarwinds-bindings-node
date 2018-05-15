f567eddb9111e24a04184178ee2dd3e797a616dd
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2018-05-15 14:31:39 -0700

    Add test for readyToSample()

--- test/reporter.test.js
+++ test/reporter.test.js
@@ -1,5 +1,9 @@
 var bindings = require('../')
 
+var real = process.env.APPOPTICS_REPORTER === 'ssl'
+  && (process.env.APPOPTICS_COLLECTOR === 'collector.appoptics.com'
+  || process.env.APPOPTICS_COLLECTOR === 'collector-stg.appoptics.com')
+
 describe('addon.reporter', function () {
   var r = new bindings.Reporter()
 
@@ -11,6 +15,19 @@ describe('addon.reporter', function () {
 
   it('should initialize oboe with empty options', function () {
     bindings.oboeInit(process.env.APPOPTICS_SERVICE_KEY, {})
+  })
+
+  it('should check if ready to sample', function () {
+    // wait 5 seconds max. This will fail if not using
+    // a real SSL reporter (collector or collector-stg).
+    var ready = r.isReadyToSample(5000)
+    ready.should.be.an.instanceof(Number)
+
+    if (real) {
+      ready.should.equal(1)
+    } else {
+      ready.should.not.equal(1)
+    }
   })
 
   it('should not crash node getting the prototype of a reporter instance', function () {
