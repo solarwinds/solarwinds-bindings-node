7c3e17804c3caba02156313dd9344b1e0c5acae9
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2019-01-09 10:29:35 -0800

    test/config - remove version tests
    - no longer test removed functions

--- test/config.test.js
+++ test/config.test.js
@@ -2,31 +2,6 @@ var bindings = require('../')
 var expect = require('chai').expect;
 
 describe('addon.config', function () {
-  var version
-  var revision
-
-  it('should get oboe\'s version', function () {
-    debugger
-    version = bindings.Config.getVersion()
-    expect(version).to.be.a('number')
-  })
-
-  it('should get oboe\'s revision', function () {
-    revision = bindings.Config.getRevision()
-    expect(revision).to.be.a('number')
-  })
-
-  it('should check valid oboe versions', function () {
-    var check = bindings.Config.checkVersion(version, revision)
-    expect(check).to.be.a('boolean')
-    expect(check).equal(true)
-  })
-
-  it('should check invalid oboe versions', function () {
-    var check = bindings.Config.checkVersion(10000, 0)
-    expect(check).to.be.a('boolean')
-    expect(check).equal(false)
-  })
 
   it('should get oboe\'s version as a string', function () {
     var version = bindings.Config.getVersionString()
