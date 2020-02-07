1efe5faa894d09a203704ea0df12389c9698d95a
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2020-02-07 09:53:49 -0800

    index.js - add metrics to addon object

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ if (process.env.NODE_ENV !== 'production') {
   try {
     const segvHandler = require('segfault-handler');
     segvHandler.registerHandler('ao-segv.log');
-  } catch (e) {};
+  } catch (e) {}
 }
 module.exports = require('bindings')('appoptics-bindings.node')
 module.exports.version = require('./package.json').version
 module.exports.init = function (sk) {
   return module.exports.oboeInit({serviceKey: sk || process.env.APPOPTICS_SERVICE_KEY});
 }
+
+try {
+  module.exports.metrics = require('bindings')('ao-metrics');
+} catch (e) {}
+
+
