e13e9ba529c4321c1a5af6ec46bf66493099cf03
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2020-02-11 11:40:08 -0800

    src/metrics/gc.cc - ignore phases
    - only record major and minor GCs

--- src/metrics/gc.cc
+++ src/metrics/gc.cc
@@ -5,14 +5,14 @@
 #include "gc.h"
 
 /* https://v8docs.nodesource.com/node-8.16/d4/da0/v8_8h_source.html#l06365
- enum GCType {
-   kGCTypeScavenge = 1 << 0,
-   kGCTypeMarkSweepCompact = 1 << 1,
-   kGCTypeIncrementalMarking = 1 << 2,
-   kGCTypeProcessWeakCallbacks = 1 << 3,
-   kGCTypeAll = kGCTypeScavenge | kGCTypeMarkSweepCompact |
-                kGCTypeIncrementalMarking | kGCTypeProcessWeakCallbacks
- };
+   enum GCType {
+     kGCTypeScavenge = 1 << 0,
+     kGCTypeMarkSweepCompact = 1 << 1,
+     kGCTypeIncrementalMarking = 1 << 2,
+     kGCTypeProcessWeakCallbacks = 1 << 3,
+     kGCTypeAll = kGCTypeScavenge | kGCTypeMarkSweepCompact |
+                  kGCTypeIncrementalMarking | kGCTypeProcessWeakCallbacks
+   };
 
  enum GCCallbackFlags {
    kNoGCCallbackFlags = 0,
@@ -28,14 +28,17 @@
 namespace ao::metrics::gc {
 using namespace v8;
 
-const int maxTypeCount = kGCTypeAll + 1;
+void set_histogram_values(hdr_histogram*, Local<Object>, uint64_t);
+
 typedef struct GCData {
 	uint64_t gcTime;
     uint64_t gcCount;
-    uint64_t typeCounts[maxTypeCount];
+    uint64_t majorCount;
+    uint64_t minorCount;
 } GCData_t;
 
-struct hdr_histogram* histogram;
+struct hdr_histogram* h_major;
+struct hdr_histogram* h_minor;
 
 GCData_t raw;
 GCData_t interval_base;
@@ -57,18 +60,30 @@ NAN_GC_CALLBACK(afterGC) {
   // convert elapsed time to μ seconds
   uint64_t et = (uv_hrtime() - gcStartTime + 500) / 1000;
 
-  // build histogram for elapsed times
-  hdr_record_values(histogram, et, 1);
+  // build histogram for elapsed times. scavenges are minor and
+  // mark/sweep/compact are major. the other two types are just
+  // phases of a major garbage collection and are typically very
+  // low values that result in a large standard deviation and lower
+  // mean if added into major garbage collections.
+  // https://stackoverflow.com/questions/60171534/what-do-v8s-incrementalmarking-and-processweakcallbacks-garbage-collections-do/60172044#60172044
+  if (type & kGCTypeMarkSweepCompact) {
+    raw.majorCount += 1;
+    interval.majorCount += 1;
+    hdr_record_values(h_major, et, 1);
+  } else if (type & kGCTypeScavenge) {
+    raw.minorCount += 1;
+    interval.minorCount += 1;
+    hdr_record_values(h_minor, et, 1);
+  } else {
+    // don't update counts or time
+    return;
+  }
 
   // keep raw up-to-date
   raw.gcCount += 1;
   raw.gcTime += et;
 
-  const int index = type & kGCTypeAll;
-  raw.typeCounts[index] += 1;
-  interval.typeCounts[index] += 1;
-
-  // keep cumulative up-to-date
+  // keep the interval data up-to-date
   interval.gcCount += 1;
   interval.gcTime += et;
 }
@@ -92,7 +107,8 @@ bool start() {
   memset(&interval_base, 0, sizeof(interval_base));
   memset(&interval, 0, sizeof(interval));
 
-  hdr_init(1, INT64_C(3600000000), 3, &histogram);
+  hdr_init(1, INT64_C(3600000000), 3, &h_major);
+  hdr_init(1, INT64_C(3600000000), 3, &h_minor);
 
   Nan::AddGCPrologueCallback(recordBeforeGC);
   Nan::AddGCEpilogueCallback(afterGC);
@@ -110,8 +126,10 @@ bool stop () {
     memset(&interval, 0, sizeof(interval));
     Nan::RemoveGCPrologueCallback(recordBeforeGC);
     Nan::RemoveGCEpilogueCallback(afterGC);
-    hdr_close(histogram);
-    histogram = nullptr;
+    hdr_close(h_major);
+    h_major = nullptr;
+    hdr_close(h_minor);
+    h_minor = nullptr;
   }
 
   return status;
@@ -129,44 +147,49 @@ bool getInterval(const v8::Local<v8::Object> obj) {
   uint64_t time = raw.gcTime - interval_base.gcTime;
   Nan::Set(obj, Nan::New("gcTime").ToLocalChecked(), Nan::New<Number>(time));
 
-  // int64_t value = hdr_value_at_percentile(h, double percentile);
-  // percentile is 99.0, 100.0, etc.
-  // double mean = hdr_mean(h); double stddev = hdr_stddev(h);
-  // int64_t min = hdr_min(h); int64_t max = hdr_max(h);
-  std::map<std::string, double>::iterator it;
-  for (it = PERCENTILES.begin(); it != PERCENTILES.end(); it++) {
-    const int64_t p = hdr_value_at_percentile(histogram, it->second);
-    Nan::Set(obj, Nan::New(it->first).ToLocalChecked(), Nan::New<Number>(p));
-  }
-  Nan::Set(obj, Nan::New("min").ToLocalChecked(), Nan::New<Number>(hdr_min(histogram)));
-  Nan::Set(obj, Nan::New("max").ToLocalChecked(), Nan::New<Number>(hdr_max(histogram)));
+  uint64_t majorCount = raw.majorCount - interval_base.majorCount;
+  uint64_t minorCount = raw.minorCount - interval_base.minorCount;
 
-  // the next two values are NaN if no GCs so default them to 0.
-  const double mean = count ? hdr_mean(histogram) : 0;
-  Nan::Set(obj, Nan::New("mean").ToLocalChecked(), Nan::New<Number>(mean));
-  const double stddev = count ? hdr_stddev(histogram) : 0;
-  Nan::Set(obj, Nan::New("stddev").ToLocalChecked(), Nan::New<Number>(stddev));
+  Local<Object> major = Nan::New<Object>();
+  Local<Object> minor = Nan::New<Object>();
 
-  Local<Object> counts = Nan::New<Object>();
-  for (int i = 0; i < maxTypeCount; i++) {
-    uint64_t count = raw.typeCounts[i] - interval_base.typeCounts[i];
-    if (count != 0) {
-      Nan::Set(counts, i, Nan::New<Number>(count));
-    }
-    // reset the base
-    interval_base.typeCounts[i] = raw.typeCounts[i];
-  }
-  Nan::Set(obj, Nan::New("gcTypeCounts").ToLocalChecked(), counts);
+  set_histogram_values(h_major, major, count);
+  set_histogram_values(h_minor, minor, count);
+
+  Nan::Set(obj, Nan::New("major").ToLocalChecked(), major);
+  Nan::Set(obj, Nan::New("minor").ToLocalChecked(), minor);
+
+  Nan::Set(major, Nan::New("count").ToLocalChecked(), Nan::New<Number>(majorCount));
+  Nan::Set(minor, Nan::New("count").ToLocalChecked(), Nan::New<Number>(minorCount));
 
   // reset the interval base values
   interval_base.gcCount = raw.gcCount;
   interval_base.gcTime = raw.gcTime;
+  interval_base.majorCount = raw.majorCount;
+  interval_base.minorCount = raw.minorCount;
 
-  // reset the histogram
-  hdr_reset(histogram);
+  // reset the histograms
+  hdr_reset(h_major);
+  hdr_reset(h_minor);
 
   return true;
 }
 
+void set_histogram_values(hdr_histogram* h, Local<Object> obj, uint64_t count) {
+  std::map<std::string, double>::iterator it;
+  for (it = PERCENTILES.begin(); it != PERCENTILES.end(); it++) {
+    const int64_t p = hdr_value_at_percentile(h, it->second);
+    Nan::Set(obj, Nan::New(it->first).ToLocalChecked(), Nan::New<Number>(p));
+  }
+
+  Nan::Set(obj, Nan::New("min").ToLocalChecked(), Nan::New<Number>(hdr_min(h)));
+  Nan::Set(obj, Nan::New("max").ToLocalChecked(), Nan::New<Number>(hdr_max(h)));
+
+  // the next two values are NaN if no GCs so default them to 0.
+  const double mean = count ? hdr_mean(h) : 0;
+  Nan::Set(obj, Nan::New("mean").ToLocalChecked(), Nan::New<Number>(mean));
+  const double stddev = count ? hdr_stddev(h) : 0;
+  Nan::Set(obj, Nan::New("stddev").ToLocalChecked(), Nan::New<Number>(stddev));
+}
 
 } // namespace ao::metrics::gc
