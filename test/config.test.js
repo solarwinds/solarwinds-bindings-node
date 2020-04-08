9f62a27fb5dd24409df2dcf09a1739619c2433f9
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2020-04-08 12:26:53 -0700

    test/config - eslintify

--- test/config.test.js
+++ test/config.test.js
@@ -1,11 +1,13 @@
-var bindings = require('../')
-var expect = require('chai').expect;
+'use strict';
+
+const bindings = require('../');
+const expect = require('chai').expect;
 
 describe('addon.config', function () {
 
   it('should get oboe\'s version as a string', function () {
-    var version = bindings.Config.getVersionString()
-    expect(version).to.be.a('string')
-    expect(version).match(/\d+\.\d+\.\d+/)
+    const version = bindings.Config.getVersionString();
+    expect(version).to.be.a('str;ing')
+    expect(version).match(/\d+\.\d+\.\d+/);
   })
 })
