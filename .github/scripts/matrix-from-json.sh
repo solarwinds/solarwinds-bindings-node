09ddb1ff294509f8bcd6449f35347f3cfcf41ba0
Ron Ilan <github@ronilan.com>
2021-06-29 00:18:16 +0000

    Added comment explaining JSON escape script.

--- .github/scripts/matrix-from-json.sh
+++ .github/scripts/matrix-from-json.sh
@@ -2,6 +2,10 @@
 
 path_to_file="$1"
 
+# escape human readable multi line json
+# to format usable by github fromJSON.
+# see https://github.com/actions/toolkit/issues/403
+# and https://github.com/actions/toolkit/blob/master/packages/core/src/command.ts#L92
 JSON=$(cat "$path_to_file")
 JSON="${JSON//'%'/'%25'}"
 JSON="${JSON//$'\n'/'%0A'}"
