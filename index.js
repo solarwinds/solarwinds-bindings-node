1603358c285e44bbc1a99b4c4d798a51c35d78c9
Ron Ilan <github@ronilan.com>
2021-07-21 16:08:26 +0000

    Auto linted core files. No manual intervention. All tests pass.

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-'use strict';
+'use strict'
 
-const binary = require('@mapbox/node-pre-gyp');
-const path = require('path');
-const pkgpath = path.resolve(path.join(__dirname, './package.json'));
-const binding_path = binary.find(pkgpath);
-const bindings = require(binding_path);
+const binary = require('@mapbox/node-pre-gyp')
+const path = require('path')
+const pkgpath = path.resolve(path.join(__dirname, './package.json'))
+const binding_path = binary.find(pkgpath)
+const bindings = require(binding_path)
 
-module.exports = bindings;
+module.exports = bindings
 
-module.exports.version = require('./package.json').version;
+module.exports.version = require('./package.json').version
 module.exports.init = function (sk) {
-  return module.exports.oboeInit({serviceKey: sk || process.env.APPOPTICS_SERVICE_KEY});
+  return module.exports.oboeInit({ serviceKey: sk || process.env.APPOPTICS_SERVICE_KEY })
 }
 
 try {
   // this is hardcoded as node-pre-gyp doesn't know about multiple targets.
-  const metrics_path = binding_path.replace('apm_bindings.node', 'ao_metrics.node');
-  module.exports.metrics = require(metrics_path);
+  const metrics_path = binding_path.replace('apm_bindings.node', 'ao_metrics.node')
+  module.exports.metrics = require(metrics_path)
 } catch (e) {
   // eslint-disable-next-line no-console
-  console.warn(`appoptics metrics disabled ${e.message}`);
+  console.warn(`appoptics metrics disabled ${e.message}`)
   // return an dummy metrics if this can't be loaded
   module.exports.metrics = {
-    start () {return true},
-    stop () {return true},
-    getMetrics () {return {}}
+    start () { return true },
+    stop () { return true },
+    getMetrics () { return {} }
   }
 }
-const Event = module.exports.Event;
+const Event = module.exports.Event
 
 //
 // augment the c++ event object with a string conversion
@@ -35,16 +35,16 @@ const Event = module.exports.Event;
 //
 Event.makeFromString = function (string) {
   if (!string || string.length != 60) {
-    return undefined;
+    return undefined
   }
-  const b = Buffer.from(string, 'hex');
+  const b = Buffer.from(string, 'hex')
   if (b.length !== 30 || b[0] !== 0x2b || b[29] & 0xFE) {
-    return undefined;
+    return undefined
   }
   // an all zero op id is not valid.
   if (string.startsWith('0'.repeat(16), 42)) {
-    return undefined;
+    return undefined
   }
 
-  return Event.makeFromBuffer(b);
+  return Event.makeFromBuffer(b)
 }
--- setup-liboboe.js
+++ setup-liboboe.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const fs = require('fs');
-const releaseInfo = require('linux-os-info');
+const fs = require('fs')
+const releaseInfo = require('linux-os-info')
 
 const dir = './oboe/'
 const soname = 'liboboe-1.0.so.0'
@@ -18,16 +18,16 @@ const deleteUnused = false
 const oboeNames = {
   linux: 'liboboe-1.0-x86_64.so.0.0.0',
   alpine: 'liboboe-1.0-alpine-x86_64.so.0.0.0',
-  alpineLibreSSL: 'liboboe-1.0-alpine-libressl-x86_64.so.0.0.0',
+  alpineLibreSSL: 'liboboe-1.0-alpine-libressl-x86_64.so.0.0.0'
 }
 
 function setupLiboboe (cb) {
-  //const version = fs.readFileSync(dir + 'VERSION', 'utf8').slice(0, -1)
+  // const version = fs.readFileSync(dir + 'VERSION', 'utf8').slice(0, -1)
 
   releaseInfo().then(info => {
     if (info.platform !== 'linux') {
       const line1 = `AppopticsApm warning: the ${info.platform} platform is not yet supported`
-      const line2 = 'see: https://docs.appoptics.com/kb/apm_tracing/supported_platforms/';
+      const line2 = 'see: https://docs.appoptics.com/kb/apm_tracing/supported_platforms/'
       const line3 = 'Contact support@appoptics.com if this is unexpected.'
       const bar = '='.repeat([line1, line2, line3].reduce((m, l) => l.length > m ? l.length : m, 0))
       /* eslint-disable no-console */
@@ -39,25 +39,25 @@ function setupLiboboe (cb) {
       process.exit(1)
       /* eslint-enable no-console */
     }
-    let versionKey = 'linux';
+    let versionKey = 'linux'
     if (info.id === 'alpine') {
-      versionKey = 'alpine';
+      versionKey = 'alpine'
       // if greater than version 3.9 then use alpine with openSSL. if less than 3.9
       // use the libreSSL version.
-      //const parts = info.version_id.split('.').map(p => parseInt(p));
-      //if (parts[0] > 3) {
+      // const parts = info.version_id.split('.').map(p => parseInt(p));
+      // if (parts[0] > 3) {
       //  versionKey = 'alpine';
-      //} else {
+      // } else {
       //  versionKey = parts[0] === 3 && parts[1] >= 9 ? 'alpine' : 'alpineLibreSSL';
-      //}
+      // }
     }
-    return versionKey;
+    return versionKey
   }).then(linux => {
     const liboboeName = oboeNames[linux]
 
     // must find the library
     if (!fs.existsSync(dir + liboboeName)) {
-      throw new Error(`cannot find ${dir}${liboboeName}`);
+      throw new Error(`cannot find ${dir}${liboboeName}`)
     }
 
     //
@@ -81,14 +81,13 @@ function setupLiboboe (cb) {
       const unusedOboe = oboeNames[linux === 'alpine' ? 'linux' : 'alpine']
       // do async so errors can easily be ignored
       fs.unlink(dir + unusedOboe, function (err) {
-        console.log(err); // eslint-disable-line no-console
+        console.log(err) // eslint-disable-line no-console
         process.exit(0)
       })
     }
     process.exit(0)
-
   }).catch(e => {
-    console.error(e); // eslint-disable-line no-console
+    console.error(e) // eslint-disable-line no-console
     process.exit(1)
   })
 }
@@ -106,7 +105,7 @@ function isSymbolicLink (name) {
 
 if (!module.parent) {
   const i = setInterval(function () {}, 100)
-  setupLiboboe(function () {clearInterval(i)})
+  setupLiboboe(function () { clearInterval(i) })
 } else {
   module.exports = setupLiboboe
 }
