9ed553651cca325630023414d515699e982fae8d
Ron Ilan <github@ronilan.com>
2021-07-28 14:59:10 -0700

    Redone dev setup.
    Everything related to dev is in dev directory.
    Dev container has build tools, docker and docker compose and multiple versions of node installed.
    Added simple way to get local "one off" containers.
    "Formalized" the secret dev reop.
    
    Three npm commands launch three shell scripts:
    1. npm run dev - starts and ubuntu based dev environment defined in a local Dockerfile. Code is mounted. This is where work should be done.
    2. npm run dev:oneoff - starts a "one off" dev environment. code is copied. This is for testing specific node/os things.
    3. npm run dev:repo:reset - runs a set of git operations that "resets" the dev repo to match the main repo.
    4. npm run dev:reinstall - removed node_modules and runs npm install. To be used after switching node versions.

---
+++ dev/repo/package-patch.js
@@ -0,0 +1,13 @@
+const pkg = require('../../package.json')
+
+const patch = {
+  name: '@appoptics/apm-binding-dev',
+  binary: {
+    ...pkg.binary,
+    staging_host: 'https://apm-appoptics-bindings-node-dev-staging.s3.amazonaws.com',
+    production_host: 'https://apm-appoptics-bindings-node-dev-production.s3.amazonaws.com'
+  }
+}
+
+// output new package.json for shell script to capture
+console.log(JSON.stringify({ ...pkg, ...patch }, null, 2))
