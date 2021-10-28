520638be55d465fdded1c7f2a9849ef4f9cb6bcb
Ron Ilan <github@ronilan.com>
2021-10-28 09:24:19 -0700

    Enabled support for liboboe mode 1 (w3c) in bindings. using minimal modification approach. no refactoring. - init options include mode and options updated to version 13. - Event::makeFromBuffer accepts either xtrace or traceparent input and acts accordingly. - getTraceSettings accepts either xtrace or traceparent input and acts accordingly. - getTraceSettings accepts a tracestate as input and passes to oboe tracing decision method. input updated to version 3. - javascript Event.makeFromString (wrapper to Event::makeFromBuffer) accepts either xtrace or traceparent input and acts accordingly.

--- index.js
+++ index.js
@@ -27,24 +27,33 @@ try {
     getMetrics () { return {} }
   }
 }
+
 const Event = module.exports.Event
 
 //
 // augment the c++ event object with a string conversion
 // function.
 //
-Event.makeFromString = function (string) {
-  if (!string || string.length != 60) { // eslint-disable-line eqeqeq
-    return undefined
-  }
-  const b = Buffer.from(string, 'hex')
-  if (b.length !== 30 || b[0] !== 0x2b || b[29] & 0xFE) {
-    return undefined
-  }
-  // an all zero op id is not valid.
-  if (string.startsWith('0'.repeat(16), 42)) {
-    return undefined
-  }
 
-  return Event.makeFromBuffer(b)
+const validXtrace = (xtrace) => {
+  // https://github.com/librato/trace/tree/master/docs/specs
+  const regExp = /\b2B[0-9A-F]{40}[0-9A-F]{16}[0-1]{2}\b/
+  const matches = regExp.exec(xtrace)
+
+  return matches ? matches[0] : ''
+}
+
+const validTraceparent = (traceparent) => {
+  // https://www.w3.org/TR/trace-context/
+  const regExp = /\b00-[0-9a-f]{32}-[0-9a-f]{16}-[0-9a-f]{2}\b/
+  const matches = regExp.exec(traceparent)
+
+  return matches
+}
+
+Event.makeFromString = function (string) {
+  if (validXtrace(string)) return Event.makeFromBuffer(Buffer.from(string, 'hex'))
+  if (validTraceparent(string)) return Event.makeFromBuffer(Buffer.from(string.replace(/-/g, ''), 'hex'))
+
+  return undefined
 }
