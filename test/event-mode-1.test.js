3a28db31f15c19175395eecb9ec2d6d9e55db01a
Ron Ilan <github@ronilan.com>
2021-12-01 22:10:45 -0800

    Changed test titles.

--- test/event-mode-1.test.js
+++ test/event-mode-1.test.js
@@ -10,7 +10,7 @@ const maxIsReadyToSampleWait = 60000
 const evUnsampled = '00-5bd5777ca0077c734b537b64c6b96921-1aa0b1b42979f5c4-00'
 const evSampled = '00-4fc9017ba3404828f253638a697dc7cf-a544d5b98159b555-01'
 
-describe('addon.event w3c', function () {
+describe('addon.event mode 1', function () {
   before(function () {
     const serviceKey = process.env.APPOPTICS_SERVICE_KEY || `${env.AO_TOKEN_NH}:node-bindings-test`
     const endpoint = process.env.APPOPTICS_COLLECTOR || `${env.APPOPTICS_COLLECTOR_NH}`
