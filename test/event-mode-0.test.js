6c20f03aaa0d9ca5d0aa8a93b5b5ee31a4507e6b
Ron Ilan <github@ronilan.com>
2022-04-04 20:44:58 -0700

    Tweak for rebase on top of AO-21080-Refactor-Tests-and-GH-Actions-for-Future-Proofing

--- test/event-mode-0.test.js
+++ test/event-mode-0.test.js
@@ -11,8 +11,8 @@ const evSampled = '2B4FC9017BA3404828F253638A697DC7CF1A6BB1A4A544D5B98159B55501'
 
 describe('bindings.Event mode 0', function () {
   before(function () {
-    const serviceKey = process.env.SOLARWINDS_SERVICE_KEY || `${env.AO_TOKEN_STG}:node-bindings-test`
-    const endpoint = process.env.SOLARWINDS_COLLECTOR || 'collector-stg.appoptics.com'
+    const serviceKey = process.env.APPOPTICS_SERVICE_KEY
+    const endpoint = process.env.APPOPTICS_COLLECTOR
 
     this.timeout(maxIsReadyToSampleWait)
     const status = bindings.oboeInit({ serviceKey, endpoint })
