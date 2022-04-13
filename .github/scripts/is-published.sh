4205004743eb47024fa44e1942d74c21daf99d06
Ron Ilan <github@ronilan.com>
2022-04-12 18:48:20 -0700

    Added package and version output to publish status script.

--- .github/scripts/is-published.sh
+++ .github/scripts/is-published.sh
@@ -3,11 +3,12 @@
 # for the name and version specified in package.json
 pkg=$(node -e "console.log(require('./package.json').name)")
 version=$(node -e "console.log(require('./package.json').version)")
+echo "Checking $version of $pkg."
 
-# check if package was already published
+# check if package version was already published
 published=$(npm view "$pkg" versions --json | grep "$version")
 
-# if it was error will halt workflow
+# if it was published - an error will halt workflow
 if [ "$published" != "" ]; then
     echo "$published already published to NPM registry."
     exit 1 
