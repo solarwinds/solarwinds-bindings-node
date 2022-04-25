3a12dd386e0928a4b3df463fc70c35884f85e565
Ron Ilan <github@ronilan.com>
2022-04-25 09:18:37 -0700

    Updated GitHub Actions nvm install script to 0.39.1

--- .github/docker-node/nvm-v0.39.1-install.sh
+++ .github/docker-node/nvm-v0.39.1-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ###
-# taken from https://raw.githubusercontent.com/nvm-sh/nvm/3fea5493a431ac64470d4230d4b51438cf213bd1/install.sh
+# taken from https://raw.githubusercontent.com/nvm-sh/nvm/9600617c52d0d2e48493424c529ac6c945d2775b/install.sh
 ###
 
 { # this ensures the entire script is downloaded #
@@ -31,7 +31,7 @@ nvm_install_dir() {
 }
 
 nvm_latest_version() {
-  nvm_echo "v0.38.0"
+  nvm_echo "v0.39.1"
 }
 
 nvm_profile_is_bash_or_zsh() {
@@ -164,11 +164,11 @@ install_nvm_from_git() {
     exit 2
   }
   if [ -n "$(command git --git-dir="$INSTALL_DIR"/.git --work-tree="$INSTALL_DIR" show-ref refs/heads/master)" ]; then
-    if command git --git-dir="$INSTALL_DIR"/.git --work-tree="$INSTALL_DIR" branch --quiet 2>/dev/null; then
-      command git --git-dir="$INSTALL_DIR"/.git --work-tree="$INSTALL_DIR" branch --quiet -D master >/dev/null 2>&1
+    if command git --no-pager --git-dir="$INSTALL_DIR"/.git --work-tree="$INSTALL_DIR" branch --quiet 2>/dev/null; then
+      command git --no-pager --git-dir="$INSTALL_DIR"/.git --work-tree="$INSTALL_DIR" branch --quiet -D master >/dev/null 2>&1
     else
       nvm_echo >&2 "Your version of git is out of date. Please update it!"
-      command git --git-dir="$INSTALL_DIR"/.git --work-tree="$INSTALL_DIR" branch -D master >/dev/null 2>&1
+      command git --no-pager --git-dir="$INSTALL_DIR"/.git --work-tree="$INSTALL_DIR" branch -D master >/dev/null 2>&1
     fi
   fi
 
@@ -271,14 +271,16 @@ nvm_detect_profile() {
   local DETECTED_PROFILE
   DETECTED_PROFILE=''
 
-  if [ -n "${BASH_VERSION-}" ]; then
+  if [ "${SHELL#*bash}" != "$SHELL" ]; then
     if [ -f "$HOME/.bashrc" ]; then
       DETECTED_PROFILE="$HOME/.bashrc"
     elif [ -f "$HOME/.bash_profile" ]; then
       DETECTED_PROFILE="$HOME/.bash_profile"
     fi
-  elif [ -n "${ZSH_VERSION-}" ]; then
-    DETECTED_PROFILE="$HOME/.zshrc"
+  elif [ "${SHELL#*zsh}" != "$SHELL" ]; then
+    if [ -f "$HOME/.zshrc" ]; then
+      DETECTED_PROFILE="$HOME/.zshrc"
+    fi
   fi
 
   if [ -z "$DETECTED_PROFILE" ]; then
@@ -302,7 +304,7 @@ nvm_detect_profile() {
 nvm_check_global_modules() {
   local NPM_COMMAND
   NPM_COMMAND="$(command -v npm 2>/dev/null)" || return 0
-  [ -n "${NVM_DIR}" ] && [ -z "${NPM_COMMAND%%$NVM_DIR/*}" ] && return 0
+  [ -n "${NVM_DIR}" ] && [ -z "${NPM_COMMAND%%"$NVM_DIR"/*}" ] && return 0
 
   local NPM_VERSION
   NPM_VERSION="$(npm --version)"
@@ -362,7 +364,7 @@ nvm_do_install() {
     # Autodetect install method
     if nvm_has git; then
       install_nvm_from_git
-    elif nvm_has nvm_download; then
+    elif nvm_has curl || nvm_has wget; then
       install_nvm_as_script
     else
       nvm_echo >&2 'You need git, curl, or wget to install nvm'
@@ -375,7 +377,7 @@ nvm_do_install() {
     fi
     install_nvm_from_git
   elif [ "${METHOD}" = 'script' ]; then
-    if ! nvm_has nvm_download; then
+    if ! nvm_has curl && ! nvm_has wget; then
       nvm_echo >&2 "You need curl or wget to install nvm"
       exit 1
     fi
