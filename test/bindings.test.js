8bec75a758a9464126aa57398efbb417eb19f81e
Raphaël Thériault <raphael.theriault@solarwinds.com>
2022-09-30 12:33:14 -0700

    test oboeInit with only a service key and no metricFormat

--- test/bindings.test.js
+++ test/bindings.test.js
@@ -41,7 +41,7 @@ describe('bindings.oboeInit()', function () {
   // tests here always run against production collector. no way to "override" with settings.
 
   it('should initialize oboe with only a service key', function () {
-    const result = bindings.oboeInit({ serviceKey: `${env.SW_TEST_PROD_SERVICE_KEY}`, metricFormat: 0 })
+    const result = bindings.oboeInit({ serviceKey: `${env.SW_TEST_PROD_SERVICE_KEY}` })
     // oboeInit can return -1 for already initialized or 0 if succeeded.
     // depending on whether this is run as part of a suite or standalone
     // either result is valid.
