ef87668b61893d4d9ac20160359f9fab51baa8d8
Raphaël Thériault <raphael.theriault@solarwinds.com>
2023-02-27 17:11:32 -0800

    improve and simplify alpine testing

--- .github/scripts/matrix-from-json.sh
+++ .github/scripts/matrix-from-json.sh
@@ -17,8 +17,8 @@ for env_var in $(printenv); do
 done
 
 JSON_WITH_ARCH=$(echo "$JSON" | jq -c '.include = [.include[] | .+{ arch: ["arm64", "x64"][] }]')
-# temporary fix for gha not supporting arm64 alpine < 3.17
-JSON_WITH_ARCH=$(echo "$JSON_WITH_ARCH" | jq -c '.include = [.include[] | select((.arch == "arm64") and (.image | contains("alpine")) and (.image | contains("alpine3.17") | not) | not)]')
+# temporary fix for gha not supporting old arm64 alpine whatsoever
+JSON_WITH_ARCH=$(echo "$JSON_WITH_ARCH" | jq -c '.include = [.include[] | select((.arch == "arm64") and (.image | contains("alpine")) and (.image | endswith("alpine") | not) | not)]')
 
 ARM64_IMAGES=$(echo "$JSON_WITH_ARCH" | jq -r '.include[] | select(.arch == "arm64") | .image')
 
